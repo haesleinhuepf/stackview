7658ff651ee9c79f12b4e08a305599849ca505cb
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-12-20 13:43:36 +0100

    deactivated modules and functions for now

--- stackview/_context.py
+++ stackview/_context.py
@@ -6,10 +6,10 @@ def nop(image):
 
 class Context():
     def __init__(self, variables):
-        self._functions = {"stackview.nop": nop}
-        self._modules = {}
+        #self._functions = {"stackview.nop": nop}
+        #self._modules = {}
         self._images = {}
-        self._label_images = {}
+        #self._label_images = {}
 
         self.parse(variables.items())
 
@@ -21,27 +21,21 @@ class Context():
 
         for name, value in items:
             if not name.startswith("_"):
-                if isinstance(value, ModuleType):
+                #if isinstance(value, ModuleType):
+                #    if prefix is None:
+                #        self._modules[name] = value
+                #        self.parse({key: getattr(value, key) for key in dir(value)}.items(), prefix=name + ".")
+                #elif isinstance(value, Callable):
+                #    if count_image_parameters(value) > 0:
+                #        if prefix is None:
+                #            self._functions[name] = value
+                #        else:
+                #            self._functions[prefix + name] = value
+                #el
+                if is_image(value):
                     if prefix is None:
-                        self._modules[name] = value
-                        self.parse({key: getattr(value, key) for key in dir(value)}.items(), prefix=name + ".")
-                elif isinstance(value, Callable):
-                    if count_image_parameters(value) > 0:
-                        if prefix is None:
-                            self._functions[name] = value
-                        else:
-                            self._functions[prefix + name] = value
-                elif is_image(value):
-                    if prefix is None:
-                        if is_label_image(value):
-                            self._label_images[name] = value
-                        else:
+                        #if is_label_image(value):
+                        #    self._label_images[name] = value
+                        #else:
                             self._images[name] = value
-
-
-
-
-
-
-
 
