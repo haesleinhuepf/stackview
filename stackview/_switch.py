dfd03811d8845d1cb11aa3ac4903f1d1340fc635
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-04-15 09:52:08 +0200

    make switch() toggleable to view multiple images with different channels/luts

--- stackview/_switch.py
+++ stackview/_switch.py
@@ -9,7 +9,8 @@ def switch(images,
            zoom_spline_order:int = 0,
            colormap:str = None,
            display_min:float = None,
-           display_max:float = None
+           display_max:float = None,
+           toggleable:bool = False
 ):
     """Allows switching between multiple images.
 
@@ -37,6 +38,9 @@ def switch(images,
         Lower bound of properly shown intensities
     display_max: float of list of float, optional
         Upper bound of properly shown intensities
+    toggleable: bool, optional
+        If true, multiple images can be shown at the same time.
+        In that case, it is recommended to pass a list as colormap as well.
 
     Returns
     -------
@@ -45,6 +49,10 @@ def switch(images,
     from ._utilities import _no_resize
     from ._slice_viewer import _SliceViewer
     import ipywidgets
+    from functools import partial
+    from ._image_widget import _img_to_rgb
+    import numpy as np
+
     if not isinstance(colormap, list):
         colormap = [colormap] * len(images)
     if not isinstance(display_min, list):
@@ -77,14 +85,49 @@ def switch(images,
     slice_slider = viewer.slice_slider
 
     buttons = []
-    for name, image, colormap_, display_min_, display_max_ in zip(names, images, colormap, display_min, display_max):
-        button = _make_button(name, image, colormap_, display_min_, display_max_, layout, viewer)
+
+    if toggleable:
+        def display_(buttons, images, colormap, display_min, display_max):
+            display_image = np.zeros([images[0].shape[0], images[0].shape[1], 3])
+            for button, image, colormap_, display_min_, display_max_ in zip(buttons, images, colormap,
+                                                                             display_min, display_max):
+                if button.value:
+                    display_image_to_add = _img_to_rgb(image, display_min=display_min_, display_max=display_max_, colormap=colormap_)
+                    if display_image is None:
+                        display_image = display_image_to_add
+                    else:
+                        display_image = display_image + display_image_to_add
+
+            display_image = np.minimum(display_image, np.asarray([[255]]))
+
+            viewer.view.colormap = None
+            viewer.view.display_min = None
+            viewer.view.display_max = None
+            viewer.image = display_image
+            viewer.configuration_updated()
+
+        display_function = partial(display_, buttons, images, colormap, display_min, display_max)
+    else:
+        def display_(image, colormap, display_min, display_max):
+            viewer.view.colormap = colormap
+            viewer.view.display_min = display_min
+            viewer.view.display_max = display_max
+            viewer.image = image
+            viewer.configuration_updated()
+
+    for i, name, image, colormap_, display_min_, display_max_ in zip(range(len(names)), names, images, colormap, display_min, display_max):
+        if toggleable:
+            button = _make_toggle_button(name, layout, True, display_function)
+        else:
+            display_function = partial(display_, image, colormap_, display_min_, display_max_)
+            button = _make_button(name, layout, display_function)
+
         buttons.append(button)
 
     return ipywidgets.VBox([_no_resize(view), ipywidgets.HBox(buttons), slice_slider])
 
 
-def _make_button(name, image, colormap, display_min, display_max, layout, viewer):
+def _make_button(name, layout, display_function):
     import ipywidgets
     if layout is None:
         button = ipywidgets.Button(description=name)
@@ -92,11 +135,20 @@ def _make_button(name, image, colormap, display_min, display_max, layout, viewer
         button = ipywidgets.Button(description=name, layout=layout)
 
     def act(event=None):
-        viewer.view.colormap = colormap
-        viewer.view.display_min = display_min
-        viewer.view.display_max = display_max
-        viewer.image = image
-        viewer.configuration_updated()
+        display_function()
 
     button.on_click(act)
+    return button
+
+def _make_toggle_button(name, layout, value, display_function):
+    import ipywidgets
+    if layout is None:
+        button = ipywidgets.ToggleButton(description=name, value=value)
+    else:
+        button = ipywidgets.ToggleButton(description=name, layout=layout, value=value)
+
+    def act(event=None):
+        display_function()
+
+    button.observe(act, 'value')
     return button
