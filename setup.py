5d2f4c4d6cc53708b0321a8ea341bf02e7ecd6b0
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-03-23 14:54:20 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stackview",
-    version="0.7.4",
+    version="0.7.5",
     author="Robert Haase",
     author_email="robert.haase@tu-dresden.de",
     description="Interactive image stack viewing in jupyter notebooks",
