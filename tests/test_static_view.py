bae04bfd192f40f44b8c9c8e4e8f584ebbcebda4
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-09-03 13:00:01 +0200

    improve numpy compatibility

--- tests/test_static_view.py
+++ tests/test_static_view.py
@@ -12,3 +12,37 @@ def test_static_view():
     assert 2 in uniques
 
     assert len(uniques) == 3
+
+def test_stackviewndarray_reshape():
+    import numpy as np
+    from stackview._static_view import StackViewNDArray
+
+    labels = StackViewNDArray(np.asarray([[0, 1], [2, 0]]))
+    new_labels = labels.reshape((1, 2, 2))
+
+    print(new_labels)
+
+    assert new_labels.shape == (1, 2, 2)
+
+def test_stackviewndarray_transpose():
+    import numpy as np
+    from stackview._static_view import StackViewNDArray
+
+    labels = StackViewNDArray(np.asarray([[0, 1], [2, 0]]))
+    new_labels = labels.transpose()
+
+    print(new_labels)
+
+    assert new_labels.shape == (2, 2)
+
+def test_stackviewndarray_expand_dims():
+    import numpy as np
+    from stackview._static_view import StackViewNDArray
+
+    labels = StackViewNDArray(np.asarray([
+        [0, 1],
+        [2, 0]
+    ]))
+
+    labels = np.expand_dims(labels, axis=0)
+    print(labels)
