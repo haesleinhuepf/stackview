a81a2a3c628852e60dfa7805b54829881a3b9562
Robert Haase <haesleinhuepf@users.noreply.github.com>
2025-02-17 18:51:28 +0100

    add bia-bob hint for displaying grids of images

--- stackview/_bia_bob_plugins.py
+++ stackview/_bia_bob_plugins.py
@@ -5,7 +5,7 @@ def list_bia_bob_plugins():
     ## Image visualization using stackview
 
     When you use stackview, you always start by importing the library: `import stackview`.
-    When asked the following tasks, you can adapt one of the correesponing code snippet:
+    When asked the following tasks, you can adapt one of the corresponding code snippet:
     * Displays an image with a slider and label showing mouse position and intensity.
       stackview.annotate(image, labels)
     
@@ -41,4 +41,11 @@ def list_bia_bob_plugins():
     
     * Allows plotting a scatterplot of a pandas dataframe in relation to a label image and optionally an image
       stackview.clusterplot(image=image, labels=label_image, df=dataframe, labels, column_x, column_y, selection_column, image)
+      
+    * Show multiple images in a grid
+      import stackview
+      from matplotlib import pyplot as plt
+      fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+      stackview.imshow(image, plot=ax[0])
+      stackview.imshow(labels, plot=ax[1]) 
     """
